1f28565bb166d5268f4933c98758305a66fdc900
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-05-01 22:12:16 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import grpc
+import MainServer_pb2, MainServer_pb2_grpc
+
+if __name__ == "__main__":
+    channel = grpc.insecure_channel('localhost:50000')
+    stub = MainServer_pb2_grpc.MainServerStub(channel)
+
+    id = int(input("Enter ID:"))
+    Text = input("Enter Text:")
+    request = MainServer_pb2.Message(Text=Text, ID=id)
+    respons = stub.SendMessage(request)
+    print("Code:", respons.Status)
+
+
